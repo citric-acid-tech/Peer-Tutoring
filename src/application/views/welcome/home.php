d970dac709a1572abf2a94be13863fb7bb5bfcb1
Peter S <petershen815@126.com>
2019-08-11 03:33:28 +0800

    2处修改 1. 上传图标库 Font Awesome v5.10.1 用于home.php的图标设计 2. 在home.php中添加图标，并设置翻转动画

--- src/application/views/welcome/home.php
+++ src/application/views/welcome/home.php
@@ -10,15 +10,17 @@
 
 <head>
 	<title>CLE Peer Tutoring | <?= $company_name ?></title>
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 
-	<link rel="icon" type="image/x-icon" href="<?= asset_url('assets/img/favicon.ico') ?>">
+	<link rel="icon" type="image/x-icon" href="<?= asset_url('assets/img/favicon.ico') ?>" />
 
-	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/bootstrap/css/bootstrap.min.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/jquery-ui/jquery-ui.min.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/jquery-qtip/jquery.qtip.min.css') ?>">
-	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/trumbowyg/ui/trumbowyg.min.css') ?>">
+	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/bootstrap/css/bootstrap.min.css') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/jquery-ui/jquery-ui.min.css') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/jquery-qtip/jquery.qtip.min.css') ?>" />
+	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/trumbowyg/ui/trumbowyg.min.css') ?>" />
+	<!-- Add Font Awesome for icons -->
+	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/ext/fontawesome/css/all.min.css') ?>" />
 
 	<link rel="stylesheet" type="text/css" href="<?= asset_url('assets/css/general.css') ?>">
 
@@ -29,6 +31,8 @@
 	<script src="<?= asset_url('assets/ext/datejs/date.js') ?>"></script>
 	<script src="<?= asset_url('assets/ext/jquery-mousewheel/jquery.mousewheel.js') ?>"></script>
 	<script src="<?= asset_url('assets/ext/trumbowyg/trumbowyg.min.js') ?>"></script>
+	<!-- Add Font Awesome for icons -->
+	<script type="text/javascript" src="<?= asset_url('assets/ext/fontawesome/js/all.min.js') ?>"></script>
 
 	<script>
 		// Global JavaScript Variables - Used in all backend pages.
@@ -94,6 +98,13 @@
 		.platform a:hover, .platform a:focus {
 			color: rgba(41, 109, 151, 1.0);
 		}
+		
+		.platform_icons {
+			transition: transform 0.2s;
+		}
+		.platform a:hover .platform_icons, .platform a:focus .platform_icons {
+			transform: rotateY(180deg);
+		}
 	</style>
 	
 </head>
@@ -118,17 +129,32 @@
 		<div class="row">
 			<div class="col-sm-4 platform">
 				<a href= <?= site_url('backend') ?> >
-					<?= lang('home_selection_admin') ?>
+					<div class="platform_icons">
+						<i class="fas fa-user-cog fa-2x"></i>
+					</div>
+					<div class="platform_texts">
+						<?= lang('home_selection_admin') ?>
+					</div>
 				</a>
 			</div>
 			<div class="col-sm-4 platform">
 				<a href= <?= site_url('appointments') ?> >
-					<?= lang('home_selection_student') ?>
+					<div class="platform_icons">
+						<i class="fas fa-user-edit fa-2x"></i>
+					</div>
+					<div class="platform_texts">
+						<?= lang('home_selection_student') ?>
+					</div>
 				</a>
 			</div>
 			<div class="col-sm-4 platform">
 				<a href= <?= site_url('backend') ?> >
-					<?= lang('home_selection_tutor') ?>
+					<div class="platform_icons">
+						<i class="fas fa-chalkboard-teacher fa-2x"></i>
+					</div>
+					<div class="platform_texts">
+						<?= lang('home_selection_tutor') ?>
+					</div>
 				</a>
 			</div>
 		</div>
