761ef62749917f3cb9401dd223a2844e16cad016
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-08-11 01:43:05 +0800

    给Welcome.php加了一些无关紧要的注释

--- src/application/controllers/Welcome.php
+++ src/application/controllers/Welcome.php
@@ -20,6 +20,9 @@ class Welcome extends CI_Controller{
     public function index(){
         
 
+        // Don't know what this about yet
+
+        
         // if ( ! $this->_has_privileges(PRIV_APPOINTMENTS))
         // {
         //     return;
@@ -28,7 +31,8 @@ class Welcome extends CI_Controller{
         $this->load->model('settings_model');
 
         $view['base_url'] = $this->config->item('base_url');
-
+        
+        // This is for the title, for more details, please see the view file (home.php)
         $view['company_name'] = $this->settings_model->get_setting('company_name');
 
         $this->load->view('welcome/home', $view);
