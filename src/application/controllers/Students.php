79c56cc087d0755f18a2ff92c6cbd47fc5e882be
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-08-11 21:51:49 +0800

    3处修改 1. 在Students控制器添加了部分用于测试的数据 2. 新增了用于Students控制器的模型：Students_model.php 3. 修改了安装数据库的sql

--- src/application/controllers/Students.php
+++ src/application/controllers/Students.php
@@ -18,26 +18,52 @@ class Students extends CI_Controller{
     }
 
     public function index(){
-        
 
-        // Don't know what this about yet
-
-        
-        // if ( ! $this->_has_privileges(PRIV_APPOINTMENTS))
-        // {
-        //     return;
-        // }
-
+        $this->load->model('appointments_model');
+        // $this->load->model('providers_model');
+        // $this->load->model('services_model');
+        // $this->load->model('customers_model');
         $this->load->model('settings_model');
+        $this->load->model('roles_model');
+        $this->load->model('user_model');
 
         $view['base_url'] = $this->config->item('base_url');
-        
-        // This is for the title, for more details, please see the view file (home.php)
+        $view['user_display_name'] = $this->user_model->get_user_display_name($this->session->userdata('user_id'));
+        $view['active_menu'] = PRIV_APPOINTMENTS;
+        $view['book_advance_timeout'] = $this->settings_model->get_setting('book_advance_timeout');
+        $view['date_format'] = $this->settings_model->get_setting('date_format');
+        $view['time_format'] = $this->settings_model->get_setting('time_format');
         $view['company_name'] = $this->settings_model->get_setting('company_name');
+        // $view['available_providers'] = $this->providers_model->get_available_providers();
+        // $view['available_services'] = $this->services_model->get_available_services();
+        // $view['customers'] = $this->customers_model->get_batch();
 
+        $user_id_test = '1'; //TODO for testing
+
+        $user = $this->user_model->get_settings($this->session->userdata($user_id_test));
+        // $view['calendar_view'] = $user['settings']['calendar_view'];
+
+        $this->set_user_data($view);
+
+        $this->load->view('students/students_home_header', $view);
         $this->load->view('students/students_home', $view);
-        
+        $this->load->view('students/students_home_footer', $view);
+    }
 
+    /**
+     * Set the user data in order to be available at the view and js code.
+     *
+     * @param array $view Contains the view data.
+     */
+    protected function set_user_data(&$view)
+    {
+        $this->load->model('roles_model');
+
+        // Get privileges
+        $view['user_id'] = $this->session->userdata('user_id');
+        $view['user_email'] = $this->session->userdata('user_email');
+        $view['role_slug'] = $this->session->userdata('role_slug');
+        $view['privileges'] = $this->roles_model->get_privileges($this->session->userdata('role_slug'));
     }
 
 }
---
+++ src/application/models/Students_model.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Students_model extends CI_Model{
+
+    public function search($booking_status, $service_type, $tutor){
+        $this->db->distinct();
+        return $this->db
+            ->select('ea_appointments.* ')
+            ->from('ea_appointments')
+            ->where('status', $booking_status)
+            ->where('id_service', $service_type)
+            ->get()->result_array();
+    }
+
+}
+
+?>
