50c6cb9b729f0ac848e71d302dc396fe8720d1b6
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-08-15 01:37:37 +0800

    修复了一个导致error500的bug：将数据库的date_format改为了date format

--- src/application/migrations/022_change_data_date_format_in_ea_setting.php
+++ src/application/migrations/022_change_data_date_format_in_ea_setting.php
@@ -5,14 +5,19 @@ class Migration_Change_data_date_format_in_ea_setting extends CI_Migration {
     public function up(){
         $data = [
             'id' => 5,
-            'name' => 'date format',
+            'name' => 'date_format',
             'value' => 'YMD'
         ];
         $this->db->replace('ea_settings', $data);
     }
 
     public function down(){
-        
+        $data = [
+            'id' => 5,
+            'name' => 'date_format',
+            'value' => 'DMY'
+        ];
+        $this->db->replace('ea_settings', $data);
     }
 
 }
