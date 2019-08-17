11fc56d5bebea956f74b8beee59205c492d919c5
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-08-17 17:01:03 +0800

    这次绝对把migration版本28修复好了

--- src/application/migrations/028_unique_index_name_in_ea_service_categories.php
+++ src/application/migrations/028_unique_index_name_in_ea_service_categories.php
@@ -3,11 +3,11 @@
 class Migration_Unique_index_name_in_ea_service_categories extends CI_Migration {
 
     public function up(){
-        $this->db->query('ALTER TABLE `ea_services` ADD UNIQUE INDEX `name_UNIQUE` (`name` ASC);');
+        $this->db->query('ALTER TABLE `ea_service_categories` ADD UNIQUE INDEX `name_UNIQUE` (`name` ASC);');
     }
 
     public function down(){
-        $this->db->query('ALTER TABLE `ea_services` DROP INDEX `name_UNIQUE` ;');
+        $this->db->query('ALTER TABLE `ea_service_categories` DROP INDEX `name_UNIQUE` ;');
     }
 }
 ?>
