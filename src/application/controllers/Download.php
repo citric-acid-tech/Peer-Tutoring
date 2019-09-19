7a67932e4c2c26c50276d23513f9b4293cda9b63
mikechesterwang <52522981+mikechesterwang@users.noreply.github.com>
2019-09-19 22:19:07 +0800

    修改了下载文件重命名

--- src/application/controllers/Download.php
+++ src/application/controllers/Download.php
@@ -10,7 +10,7 @@ class Download extends CI_Controller{
     public function index($url, $service_type, $stu_sid, $tut_sid, $start_date){
         $tmp_arr = explode('.', $url);
         $ext = count($tmp_arr) == 2 ? ('.' . $tmp_arr[1]) : '';
-        force_download($service_type . '-' . $stu_sid . '-' . $tut_sid . $start_date .$ext, file_get_contents(DOCUMENT_SAVED_PATH . $url));
+        force_download($service_type . '_' . $stu_sid . '_' . $tut_sid . '_' .$start_date .$ext, file_get_contents(DOCUMENT_SAVED_PATH . $url));
     }
 }
 ?>
