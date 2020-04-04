2b0a68e0bf6b86e481535eda33ceaf371a5ec72b
Peter S <petershen815@126.com>
2020-04-04 16:59:47 +0800

    remove OverlayScrollbars since we never use it

--- src/assets/ext/overlayScrollbar/OverlayScrollbars-master/packages/overlayscrollbars-vue/dist/types/OverlayScrollbarsComponent.vue.d.ts
+++
@@ -1,7 +0,0 @@
-import Vue, { VueConstructor } from 'vue';
-import { OverlayScrollbarsComponentData, OverlayScrollbarsComponentMethods, OverlayScrollbarsComponentComputed, OverlayScrollbarsComponentProps } from './OverlayScrollbarsComponent';
-declare const OverlayScrollbarsComponent_base: VueConstructor<OverlayScrollbarsComponentData & OverlayScrollbarsComponentMethods & OverlayScrollbarsComponentComputed & OverlayScrollbarsComponentProps & Vue>;
-export default class OverlayScrollbarsComponent extends OverlayScrollbarsComponent_base {
-    private _osInstace;
-}
-export {};
